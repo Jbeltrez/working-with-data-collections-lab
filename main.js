963702d812f27545153a955ae6c78f41e7a7a80f
john beltrez <beltrezjohn1@gmail.com>
2024-10-06 13:26:31 -0500

    part 4 completed, saved the return value of the parseCSV function into a variable, created a new function specifically for part 4, console.logged part 4 with a function call to make sure it works.

--- main.js
+++ main.js
@@ -6,17 +6,36 @@ function parseCSVrows(s) {
     const subRows = s.split('\n').splice(1, s.split('\n').length)
     // console.log(headerRow)
     // console.log(subRows)
-    const data = []
+    let data = []
     for (let i = 0; i < subRows.length; i++) {
         const row = subRows[i].split(',');
         const rowData = {};
         for (let x = 0; x < row.length; x++) {
             rowData[headerRow[x]] = row[x]
-            // console.log(rowData)
+            // console.log(rowData
         }
         data.push(rowData);
         // console.log(data)
     }
-    console.log(data)
+    return data
     }
-parseCSVrows(str)
+let x = parseCSVrows(str); 
+// console.log(x);
+
+// Part 4 sorting and manipulating data 
+// Remove the last element from the sorted array
+// Insert the following object @ index 1:
+// {id: '48', name: 'Barry', occupation: 'Runner', age: '25}
+// Add the following object to the end of the array:
+// {id: '7', name: 'Bilbo', occupation: 'None', age: '111'}
+
+function part4(arr) {
+    const newRow1 = {id: '48', name: 'Barry', occupation: 'Runner', age: '25'}
+    const newRow2 = {id: '7', name: 'Bilbo', occupation: 'None', age: '111'}
+    arr.pop();
+    arr.splice(1, 0, newRow1)
+    arr.push(newRow2);
+    return arr
+}
+console.log(part4(x))
+
